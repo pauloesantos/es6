c4dd33fcc45e8c179b0d2fe70914b7f7d432b989
Paulo Eduardo Fagundes dos Santos <paulo.esantos@gmail.com>
2017-04-04 16:29:28 -0300

    exemplos 1

---
+++ index.js
@@ -0,0 +1,19 @@
+class Message {
+    constructor(text = '', created = Date.now()) {
+        this.text = text;
+        this.created = created;
+    }
+    toString() {
+        return 'Message\'s toString';
+    }
+
+}
+
+var emptyMessage = new Message();
+var textMessage = new Message('Yesterday', Date.now() - 86400);
+
+console.log(emptyMessage);
+console.log(textMessage);
+
+console.log(emptyMessage + '');
+console.log(textMessage + '');
